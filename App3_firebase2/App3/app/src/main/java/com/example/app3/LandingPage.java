e8fdcb63b3afbfffd8d600337a9a8d1619d5d07d
Michelle Watson <michellewatson0202@gmail.com>
2022-05-01 13:55:29 -0400

    added a landing page when logging in

--- App3_firebase2/App3/app/src/main/java/com/example/app3/LandingPage.java
+++ App3_firebase2/App3/app/src/main/java/com/example/app3/LandingPage.java
@@ -1,21 +1,41 @@
 package com.example.app3;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LandingPage extends AppCompatActivity {
     private TextView greeting;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_page);
 
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        greeting.setText("Hello, " + auth.getCurrentUser().getDisplayName().toString());
+        greeting = findViewById(R.id.greeting);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Registered Users");
+
+
+
+
+
+
     }
+
 }
+
